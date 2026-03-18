0914059358fff33ba99769fe55254b828206134f
phuongnam <nnam38789@gmail.com>
2026-03-18 12:15:04 +0700

    Add .gitignore and project files

---
+++ blog_ca_nhan/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using blog_ca_nhan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace blog_ca_nhan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ blog_ca_nhan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace blog_ca_nhan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
